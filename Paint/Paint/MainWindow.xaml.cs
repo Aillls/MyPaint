0e90328e50b9de250e00973e0605930fe9d24e3b
Aillls <91529335+Aillls@users.noreply.github.com>
2021-10-04 12:43:07 +0300

    Add files via upload

--- Paint/Paint/MainWindow.xaml.cs
+++ Paint/Paint/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -28,6 +29,28 @@ namespace Paint
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             this.inkCanvas1.Strokes.Clear();
+        }
+
+        private void btn_Close_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_Save_Click(object sender, RoutedEventArgs e)
+        {
+            string imgPath = @"C:\Temp\file.gif"; //Куда сохраняется файл
+            MemoryStream ms = new MemoryStream();  //Поток памяти
+            FileStream fs = new FileStream(imgPath, FileMode.Create); //  Поток файла
+
+            //rtb - объект класса RenderTargetBitmap
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.Width, (int)inkCanvas1.Height, 100, 100, PixelFormats.Default);
+            rtb.Render(inkCanvas1);
+
+            GifBitmapEncoder gifEnc = new GifBitmapEncoder(); //сохраняем в формате GIF
+            gifEnc.Frames.Add(BitmapFrame.Create(rtb));
+            gifEnc.Save(fs);
+            fs.Close();
+            MessageBox.Show("Файл сохранен, " + imgPath); //Для информации
         }
     }
 }
