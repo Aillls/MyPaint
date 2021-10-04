01dd1fa04433365470f97308b0100771001b2695
Aillls <91529335+Aillls@users.noreply.github.com>
2021-10-04 15:36:58 +0300

    Add files via upload

--- Paint/Paint/MainWindow.xaml.cs
+++ Paint/Paint/MainWindow.xaml.cs
@@ -22,7 +22,6 @@ namespace Paint
     public partial class MainWindow : Window
     {
         public ColorRGB mcolor { get; set; }
-
         public Color clr { get; set; }
 
         public class ColorRGB
@@ -69,31 +68,50 @@ namespace Paint
             this.inkCanvas1.DefaultDrawingAttributes.Color = clr;
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            this.inkCanvas1.Strokes.Clear();
-        }
-
-        private void btn_Close_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
         private void btn_Save_Click(object sender, RoutedEventArgs e)
         {
+            this.inkCanvas1.EditingMode = InkCanvasEditingMode.None;
             string imgPath = @"C:\Temp\file.gif"; //Куда сохраняется файл
             MemoryStream ms = new MemoryStream();  //Поток памяти
             FileStream fs = new FileStream(imgPath, FileMode.Create); //  Поток файла
 
             //rtb - объект класса RenderTargetBitmap
-            RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.Width, (int)inkCanvas1.Height, 100, 100, PixelFormats.Default);
+            RenderTargetBitmap rtb = new RenderTargetBitmap((int)inkCanvas1.Width, (int)inkCanvas1.Height, 96, 96, PixelFormats.Default);
             rtb.Render(inkCanvas1);
 
             GifBitmapEncoder gifEnc = new GifBitmapEncoder(); //сохраняем в формате GIF
             gifEnc.Frames.Add(BitmapFrame.Create(rtb));
             gifEnc.Save(fs);
             fs.Close();
+            this.inkCanvas1.EditingMode = InkCanvasEditingMode.Ink;
             MessageBox.Show("Файл сохранен, " + imgPath); //Для информации
+        }
+
+        private void btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.Strokes.Clear();
+        }
+
+        private void btn_Select_Click(object sender, RoutedEventArgs e)
+        {
+            this.inkCanvas1.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void btn_AddText_Click(object sender, RoutedEventArgs e)
+        {
+            //Инициализация контрола tb типа TextBox
+            TextBox tb = new TextBox
+            {
+                Width = 100,
+                Height = 50,
+                BorderThickness = new Thickness(1),
+                //BorderBrush = new SolidColorBrush(Color.FromRgb(5, 5, 5)),
+                Margin = new Thickness(20, 20, 0, 0)
+            };
+            //Добавление контрола tb
+            this.inkCanvas1.Children.Add(tb);
+            //Переключение фокуса на элемент, чтоб можно было сразу ввести текст с клавиатуры
+            tb.Focus();
         }
     }
 }
