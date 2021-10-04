7fe30154eec1e202313cf22c1f49314b33283eb3
Aillls <91529335+Aillls@users.noreply.github.com>
2021-10-04 14:02:25 +0300

    Add files via upload

--- Paint/Paint/MainWindow.xaml.cs
+++ Paint/Paint/MainWindow.xaml.cs
@@ -21,9 +21,52 @@ namespace Paint
     /// </summary>
     public partial class MainWindow : Window
     {
+        public ColorRGB mcolor { get; set; }
+
+        public Color clr { get; set; }
+
+        public class ColorRGB
+        {
+            public byte red { get; set; }
+            public byte green { get; set; }
+            public byte blue { get; set; }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            mcolor = new ColorRGB();
+            mcolor.red = 0;
+            mcolor.green = 0;
+            mcolor.blue = 0;
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+        }
+
+        private void sld_Color_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            var slider = sender as Slider;
+            string crlName = slider.Name; //Определяем имя контрола, который покрутили
+            double value = slider.Value;  // Получаем значение контрола
+                                          //В зависимости от выбранного контрола, меняем ту или иную компоненту и переводим ее в тип byte
+            if (crlName.Equals("sld_RedColor"))
+            {
+                mcolor.red = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_GreenColor"))
+            {
+                mcolor.green = Convert.ToByte(value);
+            }
+            if (crlName.Equals("sld_BlueColor"))
+            {
+                mcolor.blue = Convert.ToByte(value);
+            }
+
+            //Задаем значение переменной класса clr 
+            clr = Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue);
+            //Устанавливаем фон для контрола Label 
+            this.lbl1.Background = new SolidColorBrush(Color.FromRgb(mcolor.red, mcolor.green, mcolor.blue));
+            // Задаем цвет кисти для контрола InkCanvas
+            this.inkCanvas1.DefaultDrawingAttributes.Color = clr;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -31,7 +74,7 @@ namespace Paint
             this.inkCanvas1.Strokes.Clear();
         }
 
-        private void btn_Close_Click_1(object sender, RoutedEventArgs e)
+        private void btn_Close_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
